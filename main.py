a72bb8750dc30c93bca5199dc27a96a78f4505d4
Michelle Dumler <shels.mail@gmail.com>
2017-01-31 21:01:50 -0600

    changed labels to tables

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ import cgi
 #def valid_email(email):
 #    return not email or EMAIL_RE.match(email)
 
-class MainHandler(webapp2.RequestHandler):
+class Index(webapp2.RequestHandler):
     """ Handles requests coming in to '/' (the root of our site)
     """
 
@@ -62,25 +62,46 @@ class MainHandler(webapp2.RequestHandler):
 
         signup_page = """
         <form action="/signup" method="post">
-            <label>
-                Username
-                <input type="text" name="username"/>
-            </label>
-            <br>
-            <label>
-                Password
-                <input type="text" name="password"/>
-            </label>
-            <br>
-            <label>
-                Verify password
-                <input type="text" name="verify_password"/>
-            </label>
-            <br>
-            <label>
-                Email (optional)
-                <input type="text" name="email"/>
-            <br>
+            <table>
+                <tr>
+                    <td class="label">
+                        Username
+                    </td>
+                    <td>
+                        <input type="text" name="username"/>
+                    <td>
+                </tr>
+            </table>
+            <table>
+                <tr>
+                    <td class="label">
+                        Password
+                    </td>
+                    <td>
+                        <input type="text" name="password"/>
+                    </td>
+                <tr>
+            </table>
+            <table>
+                <tr>
+                    <td class="label">
+                        Verify password
+                    </td>
+                    <td>
+                        <input type="text" name="verify_password"/>
+                    </td>
+                </tr>
+            </table>
+            <table>
+                <tr>
+                    <td class="label">
+                        Email (optional)
+                    </td>
+                    <td>
+                        <input type="text" name="email"/>
+                    </td>
+                <tr>
+            </table>
             <input type="submit" value="Submit"/>
         </form>
         """
@@ -91,39 +112,9 @@ class MainHandler(webapp2.RequestHandler):
         content = page_header + main_content + page_footer
         self.response.write(content)
 
-    def post(self):
-        have_error = False
-        username = self.request.get('username')
-        password = self.request.get('password')
-        verify = self.request.get('verify')
-        email = self.request.get('email')
 
-        params = dict(username = username,
-                        email = email)
-        if not valid_username(username):
-            params['error_username'] = "That's not a valid username"
-            have_error = True
-
-        if not valid_password(password):
-            params['error_password'] = "That wasn't a valid password"
-            have_error = True
-        elif password != verify:
-            params['error_verify'] = "Your passwords didn't match"
-            have_error = True
-
-        if not valid_email(email):
-            params['error_email'] = "That's not a valid email"
-            have_error = True
-
-class Welcome(webapp2.RequestHandler):
-    def get(self):
-        username = self.request.get('username')
-        if valid_username(self):
-            self.render('/welcome')
-        else:
-            self.redirect('/')
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/welcome', Welcome)
+    ('/', Index)
+
 ], debug=True)

7460c7b2f6c9b6de66293871c15895ee0f2c295f
Michelle Dumler <shels.mail@gmail.com>
2017-02-04 15:45:00 -0600

    got the errors working

--- main.py
+++ main.py
@@ -44,6 +44,8 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
+
 user_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 def valid_username(username):
     logger.warning(str(username and user_RE.match(username)))
@@ -59,24 +61,16 @@ def valid_email(email):
     logger.warning(str(not email or email_RE.match(email)))
     return not email or email_RE.match(email)
 
-
-class Index(webapp2.RequestHandler):
-    """ Handles requests coming in to '/' (the root of our site)
-    """
-
-    def get(self):
-
-#        edit_header = "<h2>Signup</h2>"
-
-        signup_page = """
-        <form action="/signup" method="post">
+signup_page = """
+<form action="/" method="post">
             <table>
                 <tr>
                     <td class="label">
                         Username
                     </td>
                     <td>
-                        <input type="text" name="username"/>
+                        <input type="text" name="username" value="%(username)s">
+                        <span class="error">%(username_error)s</span>
                     <td>
                 </tr>
             </table>
@@ -86,7 +80,8 @@ class Index(webapp2.RequestHandler):
                         Password
                     </td>
                     <td>
-                        <input type="password" name="password" value=""/>
+                        <input type="password" name="password" value="">
+                        <span class="error">%(password_error)s</span>
                     </td>
                 <tr>
             </table>
@@ -96,7 +91,8 @@ class Index(webapp2.RequestHandler):
                         Verify password
                     </td>
                     <td>
-                        <input type="password" name="verify_password" value=""/>
+                        <input type="password" name="verify_password" value="">
+                        <span class="error">%(verify_error)s</span>
                     </td>
                 </tr>
             </table>
@@ -106,62 +102,85 @@ class Index(webapp2.RequestHandler):
                         Email (optional)
                     </td>
                     <td>
-                        <input type="text" name="email"/>
+                        <input type="text" name="email" value="%(email)s">
+                        <span class="error">%(email_error)s</span>
                     </td>
                 <tr>
             </table>
             <input type="submit" value="Submit"/>
         </form>
         """
-        error = self.request.get("error")
-        error_element = "<p class='error'>" + error + "</p>" if error else ""
 
-        main_content = signup_page + error_element
-        content = page_header + main_content + page_footer
-        self.response.write(content)
+main_content = signup_page #+ error_element
+content = page_header + main_content + page_footer
 
-class Signup(webapp2.RequestHandler):
+
+class Index(webapp2.RequestHandler):
+    """ Handles requests coming in to '/' (the root of our site)
+    """
+
+
+#class Signup(webapp2.RequestHandler):
+#    def get(self):
+#        self.response.write(params)
+    def write_form(self, username="", email="", username_error="", password_error="", verify_error="",email_error=""):
+        self.response.write(content % {"username_error": username_error,
+                                            "password_error": password_error,
+                                            "verify_error": verify_error,
+                                            "email_error": email_error,
+                                            "username": username,
+                                            "email": email})
+
     def get(self):
-        self.response.write(content)
 
-    def post (self):
+#        error = self.request.get("error")
+#       error_element = "<p class='error'>" + error + "</p>" if error else ""
+        self.write_form()
+
+    def post(self):
         have_error = False
         username = self.request.get('username')
         password = self.request.get('password')
         verify = self.request.get('verify_password')
         email = self.request.get('email')
 
+        username_error = ''
+        password_error = ''
+        verify_error = ''
+        email_error = ''
+
         if not valid_username(username):
-            error = "That's not a valid username"
-            error_escaped = cgi.escape(error, quote=True)
-            self.redirect("/?error=" + error_escaped)
+            username_error = "That's not a valid username"
+            have_error = True
 
         if not valid_password(password):
-            error = "That wasn't a valid password"
-            error_escaped = cgi.escape(error, quote=True)
-            self.redirect("/?error=" + error_escaped)
-        elif password!=verify:
-            error = "Your passwords didn't match"
+            password_error = "That wasn't a valid password"
+            have_error - True
+        elif password != verify:
+            verify_error = "Your passwords didn't match"
+            have_error = True
 
         if not valid_email(email):
             logger.warning("email")
-            error = "That's not a valid email"
-            error_escaped = cgi.escape(error, quote=True)
-            self.redirect("/?error=" + error_escaped)
+            email_error = "That's not a valid email"
+            have_error = True
+
+        if have_error:
+            self.write_form(username, email, username_error, password_error, verify_error, email_error)
+        else:
+            self.redirect('/welcome?username='+ username)
 
 class Welcome(webapp2.RequestHandler):
     """ Handles requests coming in to welcome screen
     """
     def get(self):
-        username = self.request.get('username')
-        if valid_username(self):
-            sentence = "Welcome, " + username + "!"
-            welcome_content = page_header + "<p>" + sentence + "</p>" + page_footer
-            self.response.write(welcome_content)
+        username = self.request.get("username")
+        sentence = "Welcome, " + username + "!"
+        welcome_content = page_header + "<p>" + sentence + "</p>" + page_footer
+        self.response.write(welcome_content)
 
 
 app = webapp2.WSGIApplication([
     ('/', Index),
-    ('/signup', Signup),
     ('/welcome', Welcome)
 ], debug=True)
